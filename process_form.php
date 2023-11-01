c5d6a8e83cb3bcf1ad2320e1f581ca761b240961
BevanMkhabele <148946458+BevanMkhabele@users.noreply.github.com>
2023-11-01 10:10:38 +0200

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,24 +1,39 @@
 <?php
+// Database configuration
+$host = "localhost"; // Replace with your MySQL host
+$username = "your_username"; // Replace with your MySQL username
+$password = "your_password"; // Replace with your MySQL password
+$database = "registration_data"; // Replace with your database name
+
+// Create a connection to the database
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check the connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Retrieve form data
-    $name = $_POST["name"];
-    $surname = $_POST["surname"];
-    $id_number = $_POST["id_number"];
-    $school = $_POST["school"];
+    $name = $conn->real_escape_string($_POST["name"]);
+    $surname = $conn->real_escape_string($_POST["surname"]);
+    $id_number = $conn->real_escape_string($_POST["id_number"]);
+    $school = $conn->real_escape_string($_POST["school"]);
 
     // You can perform validation and sanitization of the data here as needed.
 
-    // Store the data in a file or a database
-    $data = "Name: $name\nSurname: $surname\nSA ID Number: $id_number\nSchool Name: $school\n";
+    // Insert data into the database
+    $sql = "INSERT INTO form_data (name, surname, id_number, school) VALUES ('$name', '$surname', '$id_number', '$school')";
 
-    // Define the file path where you want to save the data
-    $file_path = "form_data.txt";
-
-    // Append the data to the file (you can also use a database to store data)
-    file_put_contents($file_path, $data, FILE_APPEND);
-
-    // Redirect to a thank you page or any other page you want
-    header("Location: complete.html");
-    exit;
+    if ($conn->query($sql) === TRUE) {
+        // Redirect to a thank you page or any other page you want
+        header("Location: complete.html");
+        exit;
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
 }
+
+// Close the database connection
+$conn->close();
 ?>
