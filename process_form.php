52460ff4f600a06feed2b6fbd4c746b781924495
BevanMkhabele <148946458+BevanMkhabele@users.noreply.github.com>
2023-11-01 22:46:34 +0200

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,33 +1,25 @@
 <?php
-// Database configuration
-$host = "localhost"; // Replace with your MySQL host (e.g., localhost or your server's IP address)
+$host = "localhost"; // Replace with your MySQL host
 $username = "your_mysql_username"; // Replace with your MySQL username
 $password = "your_mysql_password"; // Replace with your MySQL password
 $database = "registration_data"; // Replace with your database name
 $table = "form_data"; // Replace with your table name
 
-// Create a connection to the database
 $conn = new mysqli($host, $username, $password, $database);
 
-// Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Retrieve form data
     $name = $conn->real_escape_string($_POST["name"]);
     $surname = $conn->real_escape_string($_POST["surname"]);
     $id_number = $conn->real_escape_string($_POST["id_number"]);
     $school = $conn->real_escape_string($_POST["school"]);
 
-    // You can perform validation and sanitization of the data here as needed.
-
-    // Insert data into the database
     $sql = "INSERT INTO $table (name, surname, id_number, school) VALUES ('$name', '$surname', '$id_number', '$school')";
 
     if ($conn->query($sql) === TRUE) {
-        // Redirect to a thank you page or any other page you want
         header("Location: complete.html");
         exit;
     } else {
@@ -35,6 +27,5 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 }
 
-// Close the database connection
 $conn->close();
 ?>
