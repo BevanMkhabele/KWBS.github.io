cd57a32f16acbd24773102ff762325bf5c9e28ba
BevanMkhabele <148946458+BevanMkhabele@users.noreply.github.com>
2023-11-01 11:10:12 +0200

    Update process_form.php

--- process_form.php
+++ process_form.php
@@ -1,9 +1,10 @@
 <?php
 // Database configuration
-$host = "localhost"; // Replace with your MySQL host
-$username = "your_username"; // Replace with your MySQL username
-$password = "your_password"; // Replace with your MySQL password
+$host = "localhost"; // Replace with your MySQL host (e.g., localhost or your server's IP address)
+$username = "your_mysql_username"; // Replace with your MySQL username
+$password = "your_mysql_password"; // Replace with your MySQL password
 $database = "registration_data"; // Replace with your database name
+$table = "form_data"; // Replace with your table name
 
 // Create a connection to the database
 $conn = new mysqli($host, $username, $password, $database);
@@ -23,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // You can perform validation and sanitization of the data here as needed.
 
     // Insert data into the database
-    $sql = "INSERT INTO form_data (name, surname, id_number, school) VALUES ('$name', '$surname', '$id_number', '$school')";
+    $sql = "INSERT INTO $table (name, surname, id_number, school) VALUES ('$name', '$surname', '$id_number', '$school')";
 
     if ($conn->query($sql) === TRUE) {
         // Redirect to a thank you page or any other page you want
